0966d1f6fdc9ac8ad1db655eb58f57041b95236a
gabriele <gabriele@192.168.1.100>
2017-07-28 22:11:19 +0200

    Commit

--- src/database/Inizializzazione.java
+++ src/database/Inizializzazione.java
@@ -1,9 +1,11 @@
 package database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 /**
  * Classe InizializzaDatabase
@@ -17,11 +19,7 @@ public class Inizializzazione {
 		
 	}
 	
-	private final static String url = "jdbc:mysql://localhost:3306/";
-	private final static String dbName = "esamiprogrammazione";
 	private final static String driver = "com.mysql.jdbc.Driver";
-	private final static String userName = "root"; 
-	private final static String password = "qrnq946";
 	
 	/**
 	 * Inizializza il database
@@ -32,19 +30,19 @@ public class Inizializzazione {
 	 */
 	public static void inizializzaDatabase() 
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
-			Class.forName(driver).newInstance();
-			Connection conn = DriverManager.getConnection(
-					url+dbName+"?autoReconnect=true&useSSL=false",userName,password
-			);
+		try {
+			InitialContext context = new InitialContext();
+			DataSource ds = (DataSource) context.lookup(driver);
+			Connection conn = ds.getConnection();
 			Statement st = conn.createStatement();
 			@SuppressWarnings("unused")
 			int res = st.executeUpdate(
 					"create database if not exists esamiprogrammazione"		
-			);
+					);
 			@SuppressWarnings("unused")
 			int res1 = st.executeUpdate(
 					"use esamiprogrammazione"
-			);
+					);
 			@SuppressWarnings("unused")
 			int res2 = st.executeUpdate(
 					"create table if not exists candidato("+
@@ -53,14 +51,14 @@ public class Inizializzazione {
 					"cognome varchar(60),"+
 					"unique (nome,cognome)"+
 					");"
-			);
+					);
 			@SuppressWarnings("unused")
 			int res3 = st.executeUpdate("create table if not exists teoria("+
 					"candidato int primary key,"+
 					"esito varchar(60),"+
 					"foreign key constaint (candidato) references candidato(id) on delete cascade"+
 					");"
-			);
+					);
 			@SuppressWarnings("unused")
 			int res4 = st.executeUpdate(
 					"create table if not exists progetto("+
@@ -70,10 +68,12 @@ public class Inizializzazione {
 					"main int,"+
 					"foreign key constaint (candidato) references candidato(id) on delete cascade"+
 					");"
-			);
-
+					);
 			st.close();
 			conn.close();
+		} catch (NamingException e) {
+			
+		}
 	}
 
 }
