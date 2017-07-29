4d6ac88e85de3866baae7d214f0813f049619e72
gabriele <gabriele@192.168.1.100>
2017-07-29 16:45:38 +0200

    Commit del 29 luglio ore 16,45

--- src/file/PrintOnFile.java
+++ src/file/PrintOnFile.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.HashSet;
-import javax.swing.JOptionPane;
 import candidati.Candidato;
 import utility.Utility;
 
