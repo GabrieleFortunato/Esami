2d54924ceae652f4419ecacb74f8b4cdfa1e11f8
gabriele <gabriele@192.168.1.100>
2017-07-30 16:44:54 +0200

    a

--- src/jFrames/InsVotoProg.java
+++ src/jFrames/InsVotoProg.java
@@ -150,7 +150,7 @@ public class InsVotoProg extends JFrame {
 		contentPane.add(votoMain);
 		votoMain.setColumns(dieci);
 		
-		JButton btnConferma = new JButton("CONFERMA1s");
+		JButton btnConferma = new JButton("CONFERMA");
 		btnConferma.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			try {
