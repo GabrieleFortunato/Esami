84e33a9cb1ffb1498c4e4adde13c547dce8490aa
gabriele <gabriele@192.168.1.100>
2017-08-03 15:51:46 +0200

    Commit del 3 agosto 2017 ore 15,51

---
+++ src/database/Cancellazione.java
@@ -0,0 +1,79 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ * Classe CancellazioneDaDatabase
+ * 
+ * @author Gabriele Fortunato
+ *
+ */
+public class Cancellazione {
+	
+	
+	/**
+	 * Localhost
+	 */
+	final static String URL = "jdbc:mysql://localhost:3306/";
+	
+	/**
+	 * Nome  del database
+	 */
+	final static String DBNAME = "esamiprogrammazione"+"?autoReconnect=true&useSSL=false";
+	
+	/**
+	 * Driver
+	 */
+	final static String DRIVER = "com.mysql.jdbc.Driver";
+
+	/**
+	 * Metodo costruttore
+	 */
+	private Cancellazione(){
+		
+	}
+	
+	/**
+	 * Cancella un candidato dal database
+	 * @param nome
+	 * @param cognome
+	 */
+	public static void cancellaCandidato(String nome, String cognome){
+		Connection conn = null;
+		PreparedStatement st = null;
+		String a = Utility.stringForQuery(nome);
+		String b = Utility.stringForQuery(cognome);
+		try {
+			conn = DriverManager.getConnection(
+					URL+DBNAME,Utility.user(),Utility.pass()
+			);
+			st = (PreparedStatement) conn.prepareStatement(
+					"delete from candidato where (nome='"+a+"' and cognome='"+b+"')"
+			);
+			int res = st.executeUpdate();
+			Logger.getLogger(Integer.toString(res));
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog (
+					null , "Problemi di connessione con il database"
+			);
+		} finally {
+			if (st!=null&&conn!=null){
+				try {
+					st.close();
+					conn.close();
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog (
+							null , "Problemi di connessione con il database"
+					);
+				}
+			}
+		}
+	} 
+}
+
+
